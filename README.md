c4fc304026121062779eb9c3734135f07c756a25
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2024-01-18 17:51:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# rainbowdream
+
+> [Running on "the rainbow dreams"](https://www.youtube.com/watch?v=UvdkDDt4tAM)
+
+CLI app for injecting custom songs into Audiosurf Radio ``.cgr`` files.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use gingerlib::channelgroups::ChannelGroup;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = "Injects a custom song into an Audiosurf Radio .cgr file.")]
+struct Args {
+    /// Path of the song to inject
+    #[arg(short, long)]
+    song_path: String,
+
+    /// Path of the .cgr file to inject into
+    #[arg(short, long)]
+    cgr_path: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let template_group = ChannelGroup::read_from_file(&args.cgr_path)?;
+    println!("Template group: {:?}", template_group.tags[0]);
+
+    Ok(())
 }
