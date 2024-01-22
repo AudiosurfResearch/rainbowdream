da3764a53a46707dd1916cc1746dded0e0c9c580
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2024-01-22 21:37:24 +0100

    .ash file autodetection!! + bump version to 1.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{ffi::OsStr, fs, path::PathBuf};
 
 use anyhow::anyhow;
 use clap::Parser;
@@ -14,11 +14,11 @@ use gingerlib::channelgroups::ChannelGroup;
 struct Args {
     /// Path of the song to inject
     #[arg(short, long)]
-    song_path: String,
+    song_path: PathBuf,
 
     /// Path of the Audiosurf highway to inject
     #[arg(short, long)]
-    ash_path: String,
+    ash_path: Option<String>,
 
     /// Path of the .cgr file to inject into
     #[arg(short, long)]
@@ -32,7 +32,40 @@ struct Args {
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let song_data = fs::read(&args.song_path)?;
-    let ash_data = fs::read(&args.ash_path)?;
+    let ash_data = match &args.ash_path {
+        Some(path) => fs::read(path)?,
+        None => {
+            println!("No .ash file specified, trying to find it in game directory");
+            let steam_dir = steamlocate::SteamDir::locate()?;
+            let (audiosurf, library) = steam_dir.find_app(12900)?.unwrap();
+            let mut audiosurf_path = library.resolve_app_dir(&audiosurf);
+            audiosurf_path.extend(["engine", "AudiosurfHC"]);
+            let mut ash_data: Vec<u8> = Vec::new();
+            // read directory
+            for entry in fs::read_dir(audiosurf_path)? {
+                let entry = entry?;
+                let path = entry.path();
+                // check if file is .ash
+                // and if the file name contains the song name
+                if path.extension() == Some(OsStr::new("ash"))
+                    && path
+                        .file_stem()
+                        .unwrap()
+                        .to_str()
+                        .unwrap()
+                        .contains(args.song_path.file_name().unwrap().to_str().unwrap())
+                {
+                    println!("Found .ash candidate at {}", path.to_str().unwrap());
+                    ash_data = fs::read(path)?;
+                    break;
+                }
+            }
+            if ash_data.is_empty() {
+                return Err(anyhow!("Couldn't find .ash file!"));
+            }
+            ash_data
+        }
+    };
 
     let mut template_group = match &args.cgr_path {
         Some(path) => ChannelGroup::read_from_file(path)?,
